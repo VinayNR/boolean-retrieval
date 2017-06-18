50184a3da52a7d53c9c6114d99a057371ca81727
Rahul Patil <rahulpatil61197@gmail.com>
2017-06-18 20:33:55 +0530

    Latest commit

--- main.cpp
+++ main.cpp
@@ -115,7 +115,7 @@ int main()
     const string str(".txt");
     DIR *dir;
     struct dirent *ent;
-    if ((dir = opendir ("D:\\gcc-codeblocks\\ir1")) != NULL)
+    if ((dir = opendir ("/Users/apple/Desktop/boolean-retrieval/Datasets")) != NULL)
     {
         while ((ent = readdir (dir)) != NULL)
         {
