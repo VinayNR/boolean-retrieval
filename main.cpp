f0efd97357a32d8eca40f522f8f3eea186339cee
N.R.Vinay <nrvinay08@gmail.com>
2017-06-28 21:48:58 +0530

    9 class classifier

--- main.cpp
+++ main.cpp
@@ -10,52 +10,12 @@
 using namespace std;
 
 vector<string> stop;
-vector<int> length(2,0);
-vector<float> prior(2);
-map<string,vector<pair<int,int> > > voc[2];
-map<string,float> prob[2];
-unsigned int docID=1,pos,classID=0;
-
-void display()
-{
-    cout<<"\nIndex-:\n\n";
-    map<string,vector<pair<int,int> > >:: iterator it;
-    voc[classID].erase(voc[classID].begin());
-    for(it=voc[classID].begin();it!=voc[classID].end();it++)
-    {
-        cout<<it->first;
-        vector<pair<int,int> >:: iterator q;
-        for(q=it->second.begin();q!=it->second.end();q++)
-            cout<<"->"<<q->first<<","<<q->second;
-        cout<<endl;
-    }
-}
-
-void extract_file(ifstream &fin)
-{
-    pos=1;
-    if (fin.is_open())
-    {
-        string word;
-        while(fin>>word)
-        {
-            for(unsigned int i=0;i<word.length();i++)
-            {
-                if(ispunct(word[i]))
-                    word.erase(word.begin()+i);
-                else if(isupper(word[i]))
-                    word[i]=tolower(word[i]);
-            }
-            if(find(stop.begin(),stop.end(),word)==stop.end())
-                voc[classID][word].push_back(make_pair(docID,pos));
-            pos++;
-        }
-        fin.close();
-        length[classID]+=pos;
-    }
-    else
-        cout <<"\nUnable to open this file:";
-}
+vector<int> length(10,0);
+vector<float> prior(10);
+map<string,vector<pair<int,int> > > voc[10];
+map<string,float> prob[10];
+unsigned int docID=1,pos,len=0;
+int classID=-2;
 
 vector<pair<int,int> > intersect(vector<pair<int,int> > p1, vector<pair<int,int> > p2)
 {
@@ -109,12 +69,51 @@ void search(string q)
         cout<<"No matching documents found!\n";
 }
 
+void display()
+{
+    cout<<"\nIndex-:\n\n";
+    map<string,vector<pair<int,int> > >:: iterator it;
+    voc[classID].erase(voc[classID].begin());
+    for(it=voc[classID].begin();it!=voc[classID].end();it++)
+    {
+        cout<<it->first;
+        vector<pair<int,int> >:: iterator q;
+        for(q=it->second.begin();q!=it->second.end();q++)
+            cout<<"->"<<q->first<<","<<q->second;
+        cout<<endl;
+    }
+}
+
+void extract_file(ifstream &fin)
+{
+    pos=1;
+    if (fin.is_open())
+    {
+        string word;
+        while(fin>>word)
+        {
+            string res;
+            remove_copy_if(word.begin(),word.end(),back_inserter(res),ptr_fun<int,int>(&ispunct));
+            for(unsigned int i=0;i<res.length();i++)
+                if(isupper(res[i]))
+                    res[i]=tolower(res[i]);
+            if(find(stop.begin(),stop.end(),res)==stop.end())
+                voc[classID][res].push_back(make_pair(docID,pos));
+            pos++;
+        }
+        fin.close();
+        length[classID]+=pos;
+    }
+    else
+        cout <<"\nUnable to open this file:";
+}
+
 void calcprob(string word, vector<float> & test)
 {
-    for(unsigned int i=0;i<classID;i++)
+    for(int i=0;i<classID;i++)
     {
         if(prob[i].find(word)==prob[i].end())
-            prob[i][word]=((float)voc[i][word].size()+1)/(length[i]+voc[0].size()+voc[1].size());
+            prob[i][word]=((float)voc[i][word].size()+1)/(length[i]+len);
         test[i]+=log(prob[i][word]);
     }
 }
@@ -122,33 +121,89 @@ void calcprob(string word, vector<float> & test)
 void classify(ifstream & fin)
 {
     string word;
-    vector<float> test(2,0.0);
-    for(unsigned int i=0;i<classID;i++)
+    //cout<<classID<<endl;
+    vector<float> test(9,0.0);
+    for(int i=0;i<classID;i++)
         test[i]+=log(prior[i]);
     if(fin.is_open())
     {
         while(fin>>word)
         {
-            for(unsigned int i=0;i<word.length();i++)
-            {
-                if(ispunct(word[i]))
-                    word.erase(word.begin()+i);
-                else if(isupper(word[i]))
-                    word[i]=tolower(word[i]);
-            }
-            calcprob(word,test);
+            string res;
+            remove_copy_if(word.begin(),word.end(),back_inserter(res),ptr_fun<int,int>(&ispunct));
+            for(unsigned int i=0;i<res.length();i++)
+                if(isupper(res[i]))
+                    res[i]=tolower(res[i]);
+            calcprob(res,test);
         }
-        cout<<"\nProbabilities-:\n";
-        cout<<test[0]<<" "<<test[1];
-        cout<<"\nThe file belongs to class "<<max_element(test.begin(), test.end()) - test.begin()+1<<endl;
+        cout<<max_element(test.begin(), test.end()) - test.begin()+1<<endl;
     }
     else
         cout<<"\nUnable to open file!";
 }
 
+void trainNB()
+{
+    DIR *dir;
+    struct dirent *ent;
+    if (((dir = opendir("C:\\Users\\Dell\\Desktop\\ir-temp\\Train\\"))) != NULL)
+    {
+        while ((ent = readdir (dir)) != NULL)
+        {
+            char S[100]="C:\\Users\\Dell\\Desktop\\ir-temp\\Train\\";
+            strcat(S,ent->d_name);
+            DIR *folder;
+            struct dirent *file;
+            if ((((folder = opendir(S))) != NULL) && classID>=0)
+            {
+                while((file = readdir(folder))!=NULL)
+                {
+                    char T[100];
+                    strcpy(T,S);
+                    strcat(T,"\\");
+                    ifstream fin(strcat(T,file->d_name));
+                    if(fin.is_open())
+                    {
+                        extract_file(fin);
+                        docID++;
+                    }
+                }
+                closedir(folder);
+            }
+            classID++;
+        }
+        closedir(dir);
+    }
+    for(int i=0;i<classID;i++)
+    {
+        prior[i]=1.0/classID;
+        len+=voc[i].size();
+    }
+}
+
+void testNB()
+{
+    DIR *dir;
+    struct dirent *ent;
+    if ((dir = opendir ("C:\\Users\\Dell\\Desktop\\ir-temp\\Test")) != NULL)
+    {
+        while ((ent = readdir (dir)) != NULL)
+        {
+            char S[100]="C:\\Users\\Dell\\Desktop\\ir-temp\\Test\\";
+            ifstream fin(strcat(S,ent->d_name));
+            if(fin.is_open())
+            {
+                cout<<ent->d_name<<" belongs to ";
+                classify(fin);
+            }
+            fin.close();
+        }
+        closedir(dir);
+    }
+}
+
 int main()
 {
-    unsigned int x;
     //----------- READ STOP WORDS ---------------------------------
     ifstream fin("stop.txt");
     if (fin.is_open())
@@ -160,82 +215,13 @@ int main()
     fin.close();
     //--------------- STOP WORDS ----------------------------------
 
-    //--------------- CLASS 1 DOCUMENTS ---------------------------
-    const string str(".txt");
-    DIR *dir;
-    struct dirent *ent;
-    if (((dir = opendir("C:\\Users\\Dell\\Desktop\\ir-temp\\Texts\\train1\\"))) != NULL)
-    {
-        while ((ent = readdir (dir)) != NULL)
-        {
-            char S[100]="C:\\Users\\Dell\\Desktop\\ir-temp\\Texts\\train1\\";
-            string s(ent->d_name);
-            //if(s.find(str)!=string::npos)
-            {
-                ifstream fin(strcat(S,s.data()));
-                if(fin.is_open())
-                    extract_file(fin);
-                //else
-                //    cout<<"\nFile not opened";
-                fin.close();
-                docID++;
-            }
-        }
-        //display();
-        closedir(dir);
-        classID++;
-        x=docID-1;
-    }
-    //------------------- CLASS 1 DOCUMENTS TAKEN -----------------------
+    trainNB();
 
-    //------------------- CLASS 2 DOCUMENTS -----------------------------
-    if ((dir = opendir ("C:\\Users\\Dell\\Desktop\\ir-temp\\Texts\\train2")) != NULL)
-    {
-        while ((ent = readdir (dir)) != NULL)
-        {
-            string s(ent->d_name);
-            char S[100]="C:\\Users\\Dell\\Desktop\\ir-temp\\Texts\\train2\\";
-            //if(s.find(str)!=string::npos)
-            {
-                ifstream fin(strcat(S,s.data()));
-                if(fin.is_open())
-                    extract_file(fin);
-                //else
-                //    cout<<"\nFile not opened";
-                fin.close();
-                docID++;
-            }
-        }
-        //display();
-        closedir(dir);
-        classID++;
-        prior[0]=(float)x/(docID-1);
-        prior[1]=1-prior[0];
-    }
-    //----------------- CLASS 2 DOCUMENTS TAKEN ------------------------
-    else
-        cout<<"\nCould not access the collection!";
+    testNB();
 
-    //----------------- TEST DOCUMENT ----------------------------------
-    if ((dir = opendir ("C:\\Users\\Dell\\Desktop\\ir-temp\\Texts\\test")) != NULL)
-    {
-        while ((ent = readdir (dir)) != NULL)
-        {
-            string s(ent->d_name);
-            char S[100]="C:\\Users\\Dell\\Desktop\\ir-temp\\Texts\\test\\";
-            //if(s.find(str)!=string::npos)
-            {
-                ifstream fin(strcat(S,s.data()));
-                if(fin.is_open())
-                    classify(fin);
-                    //cout<<ent->d_name<<endl;
-                //else
-                //    cout<<"\nFile not opened";
-                fin.close();
-            }
-        }
-    }
-    //---------------- TEXT DOCUMENT CLASSIFIED -------------------------
+    return 0;
+}
+
 
     //---------------- BOOLEAN QUERY ------------------------------------
     /*cout<<"\nEnter a Boolean Query:";
@@ -243,5 +229,3 @@ int main()
     getline(cin,q);
     search(q);*/
     //---------------- BOOLEAN QUERY ------------------------------------
-    return 0;
-}
